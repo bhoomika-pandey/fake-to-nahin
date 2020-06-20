14ac59bee268ad066e4ff7cff773209f05d23126
nilayostwal999 <nilay.ostwal999@gmail.com>
2020-06-21 01:12:39 +0530

    hi

---
+++ lib/screens/ProfileScreen.dart
@@ -0,0 +1,130 @@
+import 'package:fake_to_nahin/controllers/drawer.dart';
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: DrawerButton(),
+      appBar: AppBar(
+        title: Text('Fake To Nahin',
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
+        actions: [
+          RaisedButton(
+            onPressed: () {
+              Navigator.pushNamed(context, 'ProfileEdit');
+            },
+            child: Row(children: [Icon(Icons.edit), Text('Edit Profile')]),
+            color: Colors.green,
+            textColor: Colors.white,
+          )
+        ],
+      ),
+      body: ListView(
+          padding: EdgeInsets.all(5),
+          scrollDirection: Axis.vertical,
+          children: [
+            Container(
+                height: 200,
+                width: 200,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://www.eguardtech.com/wp-content/uploads/2018/08/Network-Profile.png')))),
+            Text('Name Variable Here',
+                style: TextStyle(
+                    color: Colors.green,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25),
+                textAlign: TextAlign.center),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 25, 0, 10),
+                child: Row(
+                  children: [
+                    Text('Username:', style: TextStyle(fontSize: 22)),
+                    Text('Username variable in line37',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    Text('City:', style: TextStyle(fontSize: 22)),
+                    Text('City Variable in line44',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    Text('State:', style: TextStyle(fontSize: 22)),
+                    Text('State Variable in line51',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    Text('Country:', style: TextStyle(fontSize: 22)),
+                    Text('Country Variable in line58',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    Text('Mobile:', style: TextStyle(fontSize: 22)),
+                    Text('Mobile Variable in line65',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                child: Row(
+                  children: [
+                    Text('E-mail ID:', style: TextStyle(fontSize: 22)),
+                    Text('E-mail ID variable in line72',
+                        style: TextStyle(
+                            fontSize: 22, fontWeight: FontWeight.bold)),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                )),
+            Card(
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                child: Row(
+                  children: [
+                    Text('Password:', style: TextStyle(fontSize: 22)),
+                    RaisedButton(
+                        onPressed: () {},
+                        child: Text('Change Password',
+                            style: TextStyle(
+                                fontSize: 22, fontWeight: FontWeight.bold))),
+                  ],
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                )),
+          ]),
+    );
+  }
+}
---
+++ lib/screens/SignInScreen.dart
@@ -0,0 +1,140 @@
+import 'package:fake_to_nahin/models/UserModel.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class SignInScreen extends StatefulWidget {
+  @override
+  _SignInScreenState createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  final emailLoginController = TextEditingController();
+  final passwordLoginController = TextEditingController();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 300,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('assets/img/app.png'))),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.person), onPressed: null),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 10),
+                        child: TextField(
+                          controller: emailLoginController,
+                          decoration: InputDecoration(hintText: 'Email'),
+                        )))
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Row(
+              children: <Widget>[
+                IconButton(icon: Icon(Icons.lock), onPressed: null),
+                Expanded(
+                    child: Container(
+                        margin: EdgeInsets.only(right: 20, left: 10),
+                        child: TextField(
+                          controller: passwordLoginController,
+                          decoration: InputDecoration(hintText: 'Password'),
+                          obscureText: true,
+                        ))),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(5),
+              child: Container(
+                height: 60,
+                child: RaisedButton(
+                  onPressed: () async {
+                    final usersCollectionRef =
+                        Firestore.instance.collection("users");
+                    final userDoc = await usersCollectionRef
+                        .document(emailLoginController.text)
+                        .get();
+
+                    // if (userDoc.data["password"] ==
+                    //     passwordLoginController.text) {
+                    //   Navigator.pushReplacementNamed(context, 'Home');
+                    // } else {
+                      return showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                            content: Text(userDoc.data.toString()),
+                          );
+                        },
+                      );
+                    }
+                  ,
+                  color: Colors.lightBlue[800],
+                  child: Text(
+                    'SIGN IN',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          InkWell(
+            onTap: () {
+              Navigator.pushNamed(context, 'SignUp');
+            },
+            child: Center(
+              child: RichText(
+                text: TextSpan(
+                    text: 'Don\'t have an account?',
+                    style: TextStyle(color: Colors.black),
+                    children: [
+                      TextSpan(
+                        text: 'SIGN UP',
+                        style: TextStyle(
+                            color: Colors.lightBlue[800],
+                            fontWeight: FontWeight.bold,
+                            fontSize: 22.0),
+                      )
+                    ]),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+// return showDialog(
+//   context: context,
+//   builder: (context) {
+//     return AlertDialog(
+//       content: Text(currentUser.firstName),
+//     );
+//   },
+// );
