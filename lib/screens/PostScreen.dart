f1adf1301bdf02f6a5497e428ead44a45efd1c6a
nilayostwal999 <nilay.ostwal999@gmail.com>
2020-06-21 16:16:28 +0530

    link pop up

--- lib/screens/PostScreen.dart
+++ lib/screens/PostScreen.dart
@@ -2,6 +2,7 @@ import 'package:fake_to_nahin/models/PostModel.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:url_launcher/url_launcher.dart';
+import '../globals.dart' as globals;
 
 class PostScreen extends StatefulWidget {
   PostModel post;
@@ -27,97 +28,128 @@ class _PostScreenState extends State<PostScreen> {
             padding: EdgeInsets.all(5),
             child: Card(
                 child: DecoratedBox(
-              decoration: BoxDecoration(
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(2),
-                  border: Border.all(
-                      color: Colors.grey,
-                      width: 2,
-                      style: BorderStyle.values[1])),
-              child: Padding(
-                  padding: EdgeInsets.all(10),
-                  child: Column(children: [
-                    RichText(
-                        text: TextSpan(
-                            text: post.title,
-                            style: TextStyle(
-                                fontSize: 26,
-                                color: Colors.lightBlue[800],
-                                fontWeight: FontWeight.bold))),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(post.username,
-                            style: TextStyle(
-                                fontSize: 22, fontWeight: FontWeight.bold)),
-                        Text(post.dateCreated,
-                            style: TextStyle(fontSize: 20, color: Colors.grey))
-                      ],
-                    ),
-                    FractionallySizedBox(
-                        widthFactor: 0.95,
-                        child: Image.network(
-                          post.mediaPath,
-                          fit: BoxFit.fitWidth,
-                        )),
-                    Text("Description\n",
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 23,
-                            fontWeight: FontWeight.bold)),
-                    RichText(
-                        text: TextSpan(
-                            text: post.description,
-                            style:
-                                TextStyle(fontSize: 18, color: Colors.black))),
-                    Container(
-                        padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
-                        child: Column(children: [
-                          Row(
-                            children: [
-                              Expanded(
-                                flex: 7,
-                                child: Text(
-                                  'Links to Resources',
-                                  style: TextStyle(
-                                      color: Colors.lightBlue[800],
-                                      fontSize: 22,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              ),
-                              Expanded(
-                                  flex: 4,
-                                  child: RaisedButton(
-                                    onPressed: () {},
-                                    child: Text(
-                                      'Post a Link',
+                    decoration: BoxDecoration(
+                        shape: BoxShape.rectangle,
+                        borderRadius: BorderRadius.circular(2),
+                        border: Border.all(
+                            color: Colors.grey,
+                            width: 2,
+                            style: BorderStyle.values[1])),
+                    child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: ListView(
+                          children: [
+                            Column(children: [
+                              RichText(
+                                  text: TextSpan(
+                                      text: post.title,
                                       style: TextStyle(
-                                          fontSize: 20, color: Colors.white),
+                                          fontSize: 26,
+                                          color: Colors.lightBlue[800],
+                                          fontWeight: FontWeight.bold))),
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(post.username,
+                                      style: TextStyle(
+                                          fontSize: 22,
+                                          fontWeight: FontWeight.bold)),
+                                  Text(post.dateCreated,
+                                      style: TextStyle(
+                                          fontSize: 20, color: Colors.grey))
+                                ],
+                              ),
+                              FractionallySizedBox(
+                                  widthFactor: 0.95,
+                                  child: Image.network(
+                                    post.mediaPath,
+                                    fit: BoxFit.fitWidth,
+                                  )),
+                              Text("Description\n",
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 23,
+                                      fontWeight: FontWeight.bold)),
+                              RichText(
+                                  text: TextSpan(
+                                      text: post.description,
+                                      style: TextStyle(
+                                          fontSize: 18, color: Colors.black))),
+                              Container(
+                                  padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                                  child: Column(children: [
+                                    Row(
+                                      children: [
+                                        Expanded(
+                                          flex: 7,
+                                          child: Text(
+                                            'Links to Resources',
+                                            style: TextStyle(
+                                                color: Colors.lightBlue[800],
+                                                fontSize: 22,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                        ),
+                                        Expanded(
+                                            flex: 4,
+                                            child: RaisedButton(
+                                              onPressed: () {
+                                                return showDialog(
+                                                  context: context,
+                                                  builder: (context) {
+                                                    return AlertDialog(
+                                                      title:
+                                                          Text('Enter a link'),
+                                                      content:Column(children: [
+                                                        TextField(
+                                                          decoration:
+                                                              InputDecoration(
+                                                                  hintText:
+                                                                      'Enter link here'),
+                                                        ),
+                                                        RaisedButton(onPressed: (){
+                                                         
+                                                        },child:Text('post'))
+                                                      ]),
+                                                    );
+                                                  },
+                                                );
+                                              },
+                                              child: Text(
+                                                'Post a Link',
+                                                style: TextStyle(
+                                                    fontSize: 20,
+                                                    color: Colors.white),
+                                              ),
+                                              color: Colors.lightBlue[800],
+                                            ))
+                                      ],
                                     ),
-                                    color: Colors.lightBlue[800],
-                                  ))
-                            ],
-                          ),
-                          StreamBuilder(
-                              stream: Firestore.instance
-                                  .collection('post')
-                                  .document()
-                                  .collection('comments')
-                                  .snapshots(),
-                              builder: (context, snapshot) {
-                                if (!snapshot.hasData)
-                                  return Text('No comments yet!!');
-                                return ListView.builder(
-                                    scrollDirection: Axis.vertical,
-                                    shrinkWrap: true,
-                                    itemCount: snapshot.data.documents.length,
-                                    itemBuilder: (context, index) =>
-                                        _buildCommentCards(context,
-                                            snapshot.data.documents[index]));
-                              })
-                        ]))
-                  ])),
-            ))));
+                                    StreamBuilder(
+                                        stream: Firestore.instance
+                                            .collection('post')
+                                            .document()
+                                            .collection('comments')
+                                            .snapshots(),
+                                        builder: (context, snapshot) {
+                                          if (!snapshot.hasData)
+                                            return Text('No comments yet!!');
+                                          return ListView.builder(
+                                              scrollDirection: Axis.vertical,
+                                              shrinkWrap: true,
+                                              itemCount: snapshot
+                                                  .data.documents.length,
+                                              itemBuilder: (context, index) =>
+                                                  _buildCommentCards(
+                                                      context,
+                                                      snapshot.data
+                                                          .documents[index]));
+                                        })
+                                  ]))
+                            ])
+                          ],
+                        ))))));
   }
 
   Widget _buildCommentCards(BuildContext context, DocumentSnapshot document) {
